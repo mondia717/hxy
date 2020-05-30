a8780ca9e333946e9ae4e5405f9397cf692fb647
mondia717 <mondia717@protonmail.com>
2020-05-31 01:01:49 +0200

    commit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
-
-
-
-
-
-
 
 let ethers = require('ethers')
 
@@ -15,7 +10,6 @@ const main = async () => {
 	let privateKey = "0x1f303ccb243d861872e049cfaa4a231ea9607871278483eea804c6ebe373f70f";
 	let wallet = new ethers.Wallet(privateKey);
 
-	// Connect a wallet to mainnet
 	let provider = ethers.getDefaultProvider();
 	let walletWithProvider = new ethers.Wallet(privateKey, provider);
 	
@@ -24,25 +18,32 @@ const main = async () => {
 	let abi = [
 		'function totalHXYTransformed() external view returns (uint256)',
 		'function balanceOf(address who) external view returns (uint256)',
-		
 	]
 	
 	let token = new ethers.Contract(ADDRESS, abi, wallet);
 	let token2 = new ethers.Contract(HEX, abi, wallet);
-	
-	
+		
 	let amount = await token.totalHXYTransformed();
 	let amount2 = await token2.balanceOf("0x44f00918a540774b422a1a340b75e055ff816d83")
 		
-		
-		
-		//
 	const server = http.createServer(function(req, res){
-	res.write(ethers.utils.formatEther(amount)*10**10 + " HXY transformed\n")
-	res.write(ethers.utils.formatEther(amount2)*10**10 + " HEX in contract")
+	res.writeHead(200, {'Content-Type': 'text/html'})
+	fs.readFile('index.html', function(error, data){
+		if(error){
+			res.writeHead(404)
+			res.write('Error: File Not Found')
+		}
+		else{
+			res.write('<!DOCTYPE html><html><title>HEY Money</title><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway"><style>body,h1 {font-family: "Raleway", sans-serif}body, html {height: 100%}.bgimg {background-color: purple;min-height: 100%;background-position: center;background-size: cover;}</style><body><div class="bgimg w3-display-container w3-animate-opacity w3-text-white"><div class="w3-display-topleft w3-padding-large w3-xlarge">HEX.business</div><div class="w3-display-middle"><h1 class="w3-large w3-center">HEX money</h1><hr class="w3-border-grey" style="margin:auto;width:40%">')
+			res.write("<p style='font-size:50px'>" + ethers.utils.formatEther(amount)*10**10 + " HXY transformed<br />")
+			res.write(ethers.utils.formatEther(amount2)*10**10 + " HEX in contract</p>")
+			res.write('<p class="w3-large w3-center">HEX money</p></div><div class="w3-display-bottomleft w3-padding-large">Haters gona hate</div></div></body></html>')
+			
+		}
+	
 		
 	res.end()
-	})
+	})})
 
 server.listen(port, function(error){
 	if (error){
@@ -52,11 +53,8 @@ server.listen(port, function(error){
 		console.log(port)
 	}	
 })
-		//
-		
-	console.log(ethers.utils.formatEther(amount)*10**10 + " HXY transformed");
-	console.log(ethers.utils.formatEther(amount2)*10**10 + " HEX in contract");
 }
+
 main();
 
 
